b23268a93ac74a338ad98bafa8b2e1fa31cab7c6
inventabuild <mrabushka@hotmail.com>
2022-10-16 15:51:04 -0500

    add heroku settings to production

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,3 +1,4 @@
+from turtle import st
 import django_on_heroku
 from decouple import config
 
@@ -12,3 +13,36 @@ DEBUG = False
 ALLOWED_HOSTS = [
     'mitch-business-apps.herokuapp.com'
 ]
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
+LOGGING = {
+    'version': 1,
+	'disable_existing_loggers': False,
+	'formatters':{
+	    'verbose': {
+	        'format' : "[%(asctime}s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+	        'datefmt' : "%d/%b/%Y %H:%M:%S"
+	    },
+	    'simple': {
+	        'format': '%(levelname)s %(message)s'
+	    },
+	},
+	'handlers': {
+	    'console': {
+	        'level': 'DEBUG',
+	        'class': 'logging.StreamHandler',
+	    },
+	},
+	'loggers': {
+	    'MYAPP': {
+	        'handlers': ['console'],
+	        'level': 'DEBUG',
+	    },
+	}
+}
+
+
+#Heroku Settings
+django_on_heroku.settings(locals(), staticfiles=False)
+del DATABASES['default']['OPTIONS']['sslmode']
