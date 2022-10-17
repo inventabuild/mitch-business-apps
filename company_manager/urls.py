ed1cab707686e486e596435426c7e75f61cfaddd
inventabuild <mrabushka@hotmail.com>
2022-10-16 19:36:07 -0500

    fixed bug conmpany_manager

--- company_manager/urls.py
+++ company_manager/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('company_options_list', views.company_options_list, name='company_options_list'),
+    path('company_new', views.company_new, name='company_new'),
 ]
