7214802b71f95ea2f2d486455a9666a335e8d175
inventabuild <mrabushka@hotmail.com>
2022-10-16 19:50:43 -0500

    finished company_manager app

--- company_manager/urls.py
+++ company_manager/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('company_options_list', views.company_options_list, name='company_options_list'),
     path('company_new', views.company_new, name='company_new'),
+    path('company_view/<int:id>', views.company_view, name='company_view'),
+    path('company_list', views.company_list, name='company_list'),
 ]
--- company_manager/views.py
+++ company_manager/views.py
@@ -26,3 +26,15 @@ def company_new(request):
         'message': message
     }
     return render(request, 'company_manager/company_new.html', data)
+def company_view(request, id):
+    company_single=models.Company.objects.get(id=id)
+    data={
+        'company_single': company_single
+    } 
+    return render(request, 'company_manager/company_view.html', data)
+def company_list(request):
+    company_all=models.Company.objects.all().order_by('name')
+    data = {
+        'company_all': company_all
+    }
+    return render(request, 'company_manager/company_list.html', data)
