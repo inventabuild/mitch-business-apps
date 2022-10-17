aeefd010f55d39540eef55920a286c8269f0f967
inventabuild <mrabushka@hotmail.com>
2022-10-16 19:29:30 -0500

    added database to company_manager

--- company_manager/views.py
+++ company_manager/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 def company_options_list(request):
     return render(request, 'company_manager/company_options_list.html')
+def company_new(request):
+    if request.method == "POST":
+        try:
+            new_company = models.Company(
+                name=request.POST.get('company_name'),
+                address=request.POST.get('company_address'),
+                city=request.POST.get('company_city'),
+                state=request.POST.get('company_state'),
+                country=request.POST.get('company_country'),
+                zip_code=request.POST.get('company_zip_code'),
+                tel=request.POST.get('company_tel'),
+            )
+            new_company.save()
+            message = 'Company successfully created'
+        except:
+            message = 'New company failed'
+    else:
+        message = ''
+    data = {
+        'message': message
+    }
+    return render(request, 'company_manager/company_new.html', data)
