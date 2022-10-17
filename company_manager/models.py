7956b0f80a3223204130b98a1cacedd7761f3a79
inventabuild <mrabushka@hotmail.com>
2022-10-16 19:22:32 -0500

    added models to company_manager

--- company_manager/models.py
+++ company_manager/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    name=models.CharField(max_length=60)
+    address=models.CharField(max_length=60)
+    city=models.CharField(max_length=60)
+    state=models.CharField(max_length=60)
+    country=models.CharField(max_length=60)
+    zip_code=models.CharField(max_length=10)
+    tel=models.CharField(max_length=20)
+
+    def _str_(self):
+        return self.title
